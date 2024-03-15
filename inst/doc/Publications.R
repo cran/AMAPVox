2a6bdf458790b5d6e646849c31c8867010d8b9f2
Philippe Verley <philippe.verley@ird.fr>
2024-03-15 22:30:21 +0000

    version 2.2.1

--- inst/doc/Publications.R
+++ inst/doc/Publications.R
@@ -1,14 +1,14 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ---- setup, echo=FALSE-------------------------------------------------------
+## ----setup, echo=FALSE--------------------------------------------------------
 # libraries RefManageR
 require("RefManageR", quietly = TRUE)  
 
-## ---- echo=FALSE, eval=TRUE, results='asis'-----------------------------------
+## ----echo=FALSE, eval=TRUE, results='asis'------------------------------------
 bib <- RefManageR::ReadBib("rsc/amapvox-bibtex.bib")
 RefManageR::NoCite(bib, "*")
 RefManageR::PrintBibliography(bib, .opts = list(style = "text", sorting = "ydnt"))
